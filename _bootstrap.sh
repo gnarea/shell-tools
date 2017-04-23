9d96dcebe5b1d1c074eac0745e6177d6f8ed2300
Gustavo Narea <me@gustavonarea.net>
2017-04-23 15:12:58 +0100

    Implement s3-object-edit

---
+++ _bootstrap.sh
@@ -0,0 +1,17 @@
+set -o nounset
+set -o errexit
+set -o pipefail
+
+
+function echo_stderr() {
+    echo "$@" >&2
+}
+
+
+function error_out() {
+    local exit_code="$1"
+    local exit_message="$2"
+
+    echo_stderr "$exit_message"
+    exit "$exit_code"
+}
