08a45eea67a6f7e6c6989afef5d306a57776153f
Gustavo Narea <gustavonarea@2degreesnetwork.com>
2017-04-24 15:02:06 +0100

    [run-n-tell] Propagate command output

--- _bootstrap.sh
+++ _bootstrap.sh
@@ -18,12 +18,3 @@ function error_out() {
     echo_stderr "$exit_message"
     exit "$exit_code"
 }
-
-
-function exec_and_return_code {
-    local command="$@"
-
-    local return_code=0
-    $@ >> /dev/null 2>&1 || return_code=$?
-    echo "${return_code}"
-}
