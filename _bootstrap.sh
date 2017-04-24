91ec96d43c076927f7efb5211318b3c2bde173b7
Gustavo Narea <gustavonarea@2degreesnetwork.com>
2017-04-24 14:02:22 +0100

    Add run-n-tell

--- _bootstrap.sh
+++ _bootstrap.sh
@@ -3,6 +3,9 @@ set -o errexit
 set -o pipefail
 
 
+# ===== Utilities
+
+
 function echo_stderr() {
     echo "$@" >&2
 }
@@ -14,4 +17,13 @@ function error_out() {
 
     echo_stderr "$exit_message"
     exit "$exit_code"
+}
+
+
+function exec_and_return_code {
+    local command="$@"
+
+    local return_code=0
+    $@ >> /dev/null 2>&1 || return_code=$?
+    echo "${return_code}"
 }
